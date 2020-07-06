6c7ca54ec5d389cd8e11bb463ade6b5d83303ebd
dkwilson <general@ytechnology.net>
2020-07-05 23:10:45 -0400

    Updates

---
+++ precache-manifest.6ec0e596f525c7a142c29360b4cd3132.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b08ec98b115a24ab8a0cd4dbe8cc457e",
+    "url": "/react_boostrap_pc/index.html"
+  },
+  {
+    "revision": "74f6375dcc3a26ed8932",
+    "url": "/react_boostrap_pc/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "49fd4c3c29f657bc4c47",
+    "url": "/react_boostrap_pc/static/js/2.af9837b4.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/react_boostrap_pc/static/js/2.af9837b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f6375dcc3a26ed8932",
+    "url": "/react_boostrap_pc/static/js/main.8f52d931.chunk.js"
+  },
+  {
+    "revision": "29f975f8101117f8706d",
+    "url": "/react_boostrap_pc/static/js/runtime-main.4d21b9e7.js"
+  },
+  {
+    "revision": "de718bf8979df13ced4f26b2fc823112",
+    "url": "/react_boostrap_pc/static/media/sky.de718bf8.jpg"
+  }
+]);
